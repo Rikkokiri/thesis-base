8d89780f4d246109636a0a7c079e2942796b1900
Ossian Rajala <ossian.rajala@gmail.com>
2024-05-26 19:54:21 +0300

    chore: fix linter config, install husky

--- src/features/candidateInfo/index.tsx
+++ src/features/candidateInfo/index.tsx
@@ -2,7 +2,7 @@ import { CandidateIntroHeader } from "./components/CandidateHeader";
 import { CandidateInfoContent } from "./components/CandidateInfoContent";
 import { useCandidateInfo } from "./hooks/useCandidateInfo";
 
-export default function ({ candidateId }: { candidateId: string }) {
+export function CandidateInfo({ candidateId }: { candidateId: string }) {
   const candidateInfo = useCandidateInfo(candidateId);
   return <CandidateInfoContent {...candidateInfo} />;
 }
