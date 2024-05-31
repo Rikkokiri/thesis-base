dfc4d0b1fdeac6bce79dc1435def611cd55ddc06
Ossian Rajala <ossian.rajala@gmail.com>
2024-05-31 18:47:12 +0300

    feat(candidate matches): reveal match toolbar on scroll

--- src/pages/QuestionsPage.tsx
+++ src/pages/QuestionsPage.tsx
@@ -1,16 +1,22 @@
 import { useTranslation } from "react-i18next";
+import { useInView } from "react-intersection-observer";
 import "./QuestionsPage.css";
 import { Button } from "../components/Button/Button";
 import { FiArrowDown } from "react-icons/fi";
 import { QuestionForm } from "../features/questionsForm";
 import { CandidatesMatch } from "src/features/candidatesMatch";
+import { getQuestionsTotalCount } from "@data/api";
 
 export const QuestionsPage = () => {
   const { t } = useTranslation();
+  const questionsTotalCount = getQuestionsTotalCount();
+  const { ref, inView } = useInView({
+    threshold: (1 / questionsTotalCount) * 1.5, // Reveal when half of second card is in view
+  });
 
   return (
     <>
-      <CandidatesMatch />
+      {inView && <CandidatesMatch />}
       <section className="question-page__header">
         <p className="subtitle m-0">{t("electionName")}</p>
         <h1 className="heading-1 pageheader_title">
@@ -22,7 +28,7 @@ export const QuestionsPage = () => {
           {t("questionPage.findYourCandidate")}
         </Button>
       </section>
-      <div className="question-page__content">
+      <div className="question-page__content" ref={ref}>
         <QuestionForm />
       </div>
     </>
